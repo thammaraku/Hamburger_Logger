3b2ed55740046de02aa8727b6432c2f49cf954a6
thamma <thammaraku@gmail.com>
2021-01-16 00:33:14 -0600

    import route

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgersController.js");
+var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
