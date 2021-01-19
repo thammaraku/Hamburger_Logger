0243473ee508731072c13395636ead8ab052bb2a
thamma <thammaraku@gmail.com>
2021-01-19 00:29:03 -0600

    add database name

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports =
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": "todolist",
+    "database": "burgers_db",
     "host": "127.0.0.1",
     "port": 3306,
     "dialect": "mysql"
