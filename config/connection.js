ee9b7bed80f1307989be579bdbd840fe9f447117
thamma <thammaraku@gmail.com>
2021-01-19 13:17:40 -0600

    revert username password

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: "127.0.0.1",
     port: "3306",
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    user: "root",
+    password: "grandkey",
     database: "burgers_db"
 });
 
