346b8ccb3fcbadc37f0b0d4ff3bccd753899ab47
thamma <thammaraku@gmail.com>
2021-01-19 11:50:01 -0600

    add config and index files

---
+++ config/config.js
@@ -0,0 +1,31 @@
+module.exports = 
+{
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": "todolist",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+}
+};
---
+++ models/index.js
@@ -0,0 +1,2 @@
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.js")[env];
