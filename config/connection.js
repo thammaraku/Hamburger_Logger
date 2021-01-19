c8339f21966a6f95708eb8fffe552fc33d202214
thamma <thammaraku@gmail.com>
2021-01-19 00:35:00 -0600

    add password

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ var mysql = require('mysql2');
 var connection = mysql.createConnection({
     host: "localhost",
     port: "3306",
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    user: "root",
+    password: "grandkey",
     database: "burgers_db"
 });
 
